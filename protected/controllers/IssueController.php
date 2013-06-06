dca64d46ab5f63b3ca6edfd459d0f63ff23527ae
chenhw <chenhongwei1112@163.com>
2013-06-06 13:12:02 +0800

    修改bug.写错,导致变量无法识别.

--- protected/controllers/IssueController.php
+++ protected/controllers/IssueController.php
@@ -101,7 +101,7 @@ class IssueController extends Controller
 	public function actionCreate()
 	{
 		$model=new Issue;
-		$model->project_id = $this->_project_id;
+		$model->project_id = $this->_project->id;
 
 		// Uncomment the following line if AJAX validation is needed
 		// $this->performAjaxValidation($model);
