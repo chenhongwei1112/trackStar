91c1f190d218d1fec86385e5b4d2f18142e78783
chenhw <chenhongwei1112@163.com>
2013-06-06 13:50:00 +0800

    修改sql脚本中的注释符号为"-- "

--- protected/test.sql
+++ protected/test.sql
@@ -1,10 +1,10 @@
---插入测试用户两人
+-- 插入测试用户两人
 INSERT INTO tbl_user (email, username, password) VALUES 
 ('test1@notanaddress.com','User One', MD5('test1')), 
 ('test2@notanaddress.com','User Two', MD5('test2'))
 ;
 
---将上文插入的两个用户关联到项目1中
+-- 将上文插入的两个用户关联到项目1中
 INSERT INTO tbl_project_user_assignment (project_id, user_id) VALUES 
 (1,1), 
 (1,2)
