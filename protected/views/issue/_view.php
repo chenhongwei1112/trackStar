f2c4f6a8cf39407ba5c92a5822722d8fb58ec46d
chenhw <chenhongwei1112@163.com>
2013-06-06 15:34:24 +0800

    修改issue显示页中的某些项,让其显示文本

--- protected/views/issue/_view.php
+++ protected/views/issue/_view.php
@@ -14,11 +14,11 @@
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('type_id')); ?>:</b>
-	<?php echo CHtml::encode($data->type_id); ?>
+	<?php echo CHtml::encode($data->getTypeText()); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('status_id')); ?>:</b>
-	<?php echo CHtml::encode($data->status_id); ?>
+	<?php echo CHtml::encode($data->getStatusText()); ?>
 	<br />
 
 	<?php /*
