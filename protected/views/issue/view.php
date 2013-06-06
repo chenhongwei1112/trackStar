50aa16b9a95ef5b53b01c544ec498274a135f2ac
chenhw <chenhongwei1112@163.com>
2013-06-06 14:59:10 +0800

    为创建issue的链接自动指定pid

--- protected/views/issue/view.php
+++ protected/views/issue/view.php
@@ -9,7 +9,7 @@ $this->breadcrumbs=array(
 
 $this->menu=array(
 	array('label'=>'List Issue', 'url'=>array('index')),
-	array('label'=>'Create Issue', 'url'=>array('create')),
+	array('label'=>'Create Issue', 'url'=>array('create','pid'=>$model->project->id)),
 	array('label'=>'Update Issue', 'url'=>array('update', 'id'=>$model->id)),
 	array('label'=>'Delete Issue', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
 	array('label'=>'Manage Issue', 'url'=>array('admin')),
