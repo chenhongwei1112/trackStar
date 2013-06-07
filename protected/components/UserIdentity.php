f5f0322e1abda90e1bb1ec8cbc83b416c18b4fd1
chenhw <chenhongwei1112@163.com>
2013-06-07 17:13:25 +0800

    1.修改用户的校验规则.从实际的数据库中提取数据校验.

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -1,4 +1,4 @@
-<?php
+<?php	
 
 /**
  * UserIdentity represents the data needed to identity a user.
@@ -15,19 +15,29 @@ class UserIdentity extends CUserIdentity
 	 * against some persistent user identity storage (e.g. database).
 	 * @return boolean whether authentication succeeds.
 	 */
-	public function authenticate()
-	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
-			$this->errorCode=self::ERROR_NONE;
-		return !$this->errorCode;
-	}
+  private $_id;
+	 
+  public function authenticate()
+  {
+    $user=User::model()->find('LOWER(username)=?',array(strtolower($this->username)));
+    if($user===null)
+      $this->errorCode=self::ERROR_USERNAME_INVALID;
+    else if(!$user->validatePassword($this->password))
+      $this->errorCode=self::ERROR_PASSWORD_INVALID;
+    else
+    {
+      $this->_id=$user->id;
+      $this->username=$user->username;
+			$this->setState('lastLogin', date("m/d/y g:i A", strtotime($user->last_login_time)));
+      $user->saveAttributes(array(
+        'last_login_time'=>date("Y-m-d H:i:s", time()),
+      ));
+      $this->errorCode=self::ERROR_NONE;
+    }
+    return $this->errorCode==self::ERROR_NONE;
+  }
+  public function getId()
+  {
+    return $this->_id;
+  }
 }
