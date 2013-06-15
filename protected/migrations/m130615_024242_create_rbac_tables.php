8f504299d7b824168a5e1337cc56787faa687d46
chenhw <chenhongwei1112@163.com>
2013-06-15 10:43:33 +0800

    创建存取权限信息的数据库表

---
+++ protected/migrations/m130615_024242_create_rbac_tables.php
@@ -0,0 +1,25 @@
+<?php
+
+class m130615_024242_create_rbac_tables extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m130615_024242_create_rbac_tables does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
