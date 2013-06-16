d883b0ccadb9453a02e6ed300634b94ecca5b595
chenhw <chenhongwei1112@163.com>
2013-06-16 14:30:18 +0800

    添加使用gii生成的comment 的 model 和crud相关代码

---
+++ protected/views/comment/_form.php
@@ -0,0 +1,60 @@
+<?php
+/* @var $this CommentController */
+/* @var $model Comment */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'comment-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'content'); ?>
+		<?php echo $form->textArea($model,'content',array('rows'=>6, 'cols'=>50)); ?>
+		<?php echo $form->error($model,'content'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'issue_id'); ?>
+		<?php echo $form->textField($model,'issue_id'); ?>
+		<?php echo $form->error($model,'issue_id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'create_time'); ?>
+		<?php echo $form->textField($model,'create_time'); ?>
+		<?php echo $form->error($model,'create_time'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'create_user_id'); ?>
+		<?php echo $form->textField($model,'create_user_id'); ?>
+		<?php echo $form->error($model,'create_user_id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'update_time'); ?>
+		<?php echo $form->textField($model,'update_time'); ?>
+		<?php echo $form->error($model,'update_time'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'update_user_id'); ?>
+		<?php echo $form->textField($model,'update_user_id'); ?>
+		<?php echo $form->error($model,'update_user_id'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
